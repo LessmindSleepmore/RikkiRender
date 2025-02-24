a1fb6dae993acdd337832adb7f44a751084f01fd
unknown <Padid@asoul.com>
2025-02-24 12:37:05 +0800

    .obj parser

--- OBJParser.h
+++ OBJParser.h
@@ -4,11 +4,19 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include "GeometricTypes.h"
 
 class OBJParser {
+private:
+	std::vector<vec3f> verts;
+	std::vector<vec3i> faces;
+
+public:
 	OBJParser(const char* filename);
 	~OBJParser();
 
-private:
-	std::vector vert<Vec3f> verts;
+	int nVerts();
+	int nFaces();
+	vec3f getVert(int i);
+	vec3i getFace(int i);
 };
