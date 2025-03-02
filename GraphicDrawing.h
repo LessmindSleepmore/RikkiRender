37d81775cfe8629df05d7b4ca4fcdb8b200c1d13
unknown <Padid@asoul.com>
2025-03-02 20:01:32 +0800

    Update .obj files parser(support .mtl files) ,add texture effect.

--- GraphicDrawing.h
+++ GraphicDrawing.h
@@ -3,6 +3,7 @@
 #include "tgaimage.h"
 #include "CustomAlgorithm.h"
 #include "GeometricTypes.h"
+#include "OBJParser.h"
 #include <algorithm>
 #include <vector>
 
@@ -10,4 +11,12 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color);
 
 void triangle(vec2i v0, vec2i v1, vec2i v2, TGAImage& image, TGAColor color);
 
-void rasterize(std::vector<vec3f> scpos, std::vector<vec3f> vertex_normals, TGAImage& image, TGAColor color, float* zbuffer, vec2i resolution, vec3f lightdir);
+void rasterize(std::vector<vec3f> scpos,
+    std::vector<vec3f> vertex_normals,
+    std::vector<vec2f> vertex_uv, TGAImage& image,
+    OBJParser& objparser,
+    int textureIdx,
+    TGAColor defualtcolor,
+    float* zbuffer,
+    vec2i resolution,
+    vec3f lightdir);
--- OBJParser.h
+++ OBJParser.h
@@ -1,30 +1,50 @@
 #pragma once
+
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <unordered_map>
 #include "GeometricTypes.h"
 
 class OBJParser {
 private:
 	std::vector<vec3f> verts;
 	std::vector<vec3f> normals;
+	std::vector<vec2f> uv;
+	std::vector<std::string> materials;
+	std::vector<unsigned char*> textures;
+	std::vector<std::vector<int>> textures_params;
+	std::unordered_map<int, int> block2texture;
 
 	/**
+	* 层次：第i块部分-第j个面片-三个顶点的顶点索引，UV索引和法线索引。
+	* 
 	* x:vertex index
 	* y:uv index
 	* z:normal index
 	*/
-	std::vector<std::vector<vec3i>> faces;
+	std::vector<std::vector<std::vector<vec3i>>> faces;
+
+	void getTexture(const char* filename);
 
 public:
 	OBJParser(const char* filename);
 	~OBJParser();
 
 	int nVerts();
-	int nFaces();
+	int nFaces(int block);
+	int nBlock();
+
+	/**
+	* Input: blockidx-块索引,uv-归一化uv坐标.
+	* Output: vec4<unsigned char>,范围0-255.
+	*/
+	vec4c samplerTexture2D(int blockidx, vec2f uv);
+	int fromBlockIdx2TextureIdx(int blockidx);
 	vec3f getVert(int i);
 	vec3f getNormals(int i);
-	std::vector<vec3i> getFace(int i);
+	vec2f getUV(int i);
+	std::vector<vec3i> getFace(int block, int i);
 };
